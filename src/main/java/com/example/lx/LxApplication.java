5bf62249bdda653e91c891e3bf7314c11e88f484
李祥 <1256858666@qq.com>
2020-05-09 16:54:05 +0800

    提交

---
+++ src/main/java/com/example/lx/LxApplication.java
@@ -0,0 +1,13 @@
+package com.example.lx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lx/LxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
